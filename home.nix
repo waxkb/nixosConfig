62e6a1eadac5c834f967f6bf16fde99ab5916e6f
waxkb <maxwellr2028@gmail.com>
2026-03-25 19:52:05 -0700

    e

--- home.nix
+++ home.nix
@@ -12,37 +12,4 @@
     };
     enable = true;
   };
-  # 1. Create the template file so the agent knows HOW to write the key
-  home.file.".ssh/github_infisical.tmpl".text = ''
-    {{- with secret "PrivateMainsshKey" -}}
-    {{ .Value }}
-    {{- end -}}
-  '';
-
-  # 2. Tell NixOS to run the Infisical Agent as a background service
-  systemd.user.services.infisical-agent = {
-    Unit = {
-      Description = "Infisical Agent for SSH Keys";
-      After = [ "network.target" ];
-    };
-    Service = {
-      # Replace 'YOUR_PROJECT_ID' with the ID from your Infisical dashboard URL
-      ExecStart = "${pkgs.infisical}/bin/infisical agent --env=dev --path=/ssh-keys --template-path=${config.home.homeDirectory}/.ssh/github_infisical.tmpl --output-path=${config.home.homeDirectory}/.ssh/github_infisical";
-      Restart = "always";
-    };
-    Install = {
-      WantedBy = [ "default.target" ];
-    };
-  };
-
-  # 3. Tell Git to use this specific key for GitHub
-  programs.ssh = {
-    enable = true;
-    matchBlocks = {
-      "github.com" = {
-        hostname = "github.com";
-        identityFile = "~/.ssh/github_infisical";
-      };
-    };
-  };
 }
