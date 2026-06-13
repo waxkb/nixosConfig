3d5c5dd42278a5c78ea0f3821b759ef69c958c3d
waxkb <maxwellr2028@gmail.com>
2026-06-12 20:40:41 -0700

    e

--- configuration.nix
+++ configuration.nix
@@ -33,6 +33,13 @@ in
     defaultEditor = true;
   };
 
+  programs.nh = {
+    enable = true;
+    clean.enable = true;
+    clean.extraArgs = "--keep-since 4d --keep 3";
+    flake = "/home/user/my-nixos-config"; # sets NH_OS_FLAKE variable for you
+  };
+
   programs.ccache = {
     enable = true;
     owner = "root";
